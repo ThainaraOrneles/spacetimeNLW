eafa61bed881c9628d796f477fd354fe42fe9bbd
Thainara Oneles <orneles.thainara23@gmail.com>
2023-05-19 19:06:55 -0300

    Fluxo de autenticação e Github OAuth config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
+
+app.register(cors, {
+  //colocar todas as URls de front-end que poderão acessar o back-end
+  //Ex. origin: ['http://localhost:3000', 'http://exemplo.com.br']
+  //o true significa que qualquer front-end poderá acessar 
+  origin: true,
+})
+
 app.register(memoriesRoutes)
 
 app.listen({
